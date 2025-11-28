bea5f92aa72526ec78ca581406fbccc47b43bda5
adamm13 <13.adamm@gmail.com>
2025-11-28 14:28:33 -0500

    WP Block Gutenberg creation

--- Blockylicious.php
+++ Blockylicious.php
@@ -1,14 +1,15 @@
 <?php
 /**
- * Plugin Name:       Curvy
- * Description:       Example block scaffolded with Create Block tool.
+ * Plugin Name:       Blockylicious
+ * Description:       A plugin of funky blocks.
  * Version:           0.1.0
  * Requires at least: 6.7
  * Requires PHP:      7.4
- * Author:            The WordPress Contributors
+ * Author:            Adam Mohammed
+ * Author URI:        https://github.com/adamm13
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       curvy
+ * Text Domain:       blockylicious
  *
  * @package CreateBlock
  */
@@ -24,7 +25,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @see https://make.wordpress.org/core/2025/03/13/more-efficient-block-type-registration-in-6-8/
  * @see https://make.wordpress.org/core/2024/10/17/new-block-type-registration-apis-to-improve-performance-in-wordpress-6-7/
  */
-function create_block_curvy_block_init() {
+function create_block_Blockylicious_block_init() {
 	/**
 	 * Registers the block(s) metadata from the `blocks-manifest.php` and registers the block type(s)
 	 * based on the registered block metadata.
@@ -53,7 +54,7 @@ function create_block_curvy_block_init() {
 	 */
 	$manifest_data = require __DIR__ . '/build/blocks-manifest.php';
 	foreach ( array_keys( $manifest_data ) as $block_type ) {
-		register_block_type( __DIR__ . "/build/{$block_type}" );
+		register_block_type( __DIR__ . "/build/blocks/curvy{$block_type}" );
 	}
 }
-add_action( 'init', 'create_block_curvy_block_init' );
+add_action( 'init', 'create_block_Blockylicious_block_init' );
